30c71b50bfb6d3112d7ae66fde32ac0d01f664b7
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-07 20:24:07 +0600

    main problem

---
+++ free.js
@@ -0,0 +1,11 @@
+const bargar = 400;
+const coke = 30;
+
+if (bargar >= 500) {
+  bargar+coke == bargar;
+  console.log(bargar)
+}
+else {
+  bargarCOke = bargar + coke;
+  console.log(bargarCOke)
+}
